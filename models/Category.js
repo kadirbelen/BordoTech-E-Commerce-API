31ba3618adf9bb82545ef855eaa2d1b9ba2087f6
kadirbelen <61799635+kadirbelen@users.noreply.github.com>
2022-07-05 10:16:12 +0300

    category and product crud operations has done

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const Category = require("./Product");
 
 const CategorySchema = new mongoose.Schema({
     categoryName: { type: String, required: true },
+    // products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,8 +3,25 @@ const Product = require("../models/Product");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("products");
+router.get("/getAll", (req, res) => {
+    Product.find()
+        .then((products) => {
+            res.json(products);
+        })
+        .catch((e) => {
+            res.json(e);
+        });
+});
+
+router.get("/getByCategoryId/:id", (req, res) => {
+    console.log(req.params.id);
+    Product.find({ id: req.params.id })
+        .then((product) => {
+            res.json(product);
+        })
+        .catch((e) => {
+            res.json(e);
+        });
 });
 
 router.post("/create", (req, res) => {
@@ -17,6 +34,31 @@ router.post("/create", (req, res) => {
 
     product.save();
     res.json(product);
+});
+
+router.put("/update/:id", (req, res) => {
+    Product.findByIdAndUpdate(req.params.id, {
+            productTitle: req.body.productTitle,
+            productDescription: req.body.productDescription,
+            productPrice: req.body.productPrice,
+            category: req.body.category,
+        })
+        .then((product) => {
+            res.json(product);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+router.delete("/delete/:id", (req, res) => {
+    Product.findByIdAndRemove(req.params.id)
+        .then((product) => {
+            res.json(product);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 module.exports = router;
