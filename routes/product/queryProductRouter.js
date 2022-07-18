99910fabf51f9d8f8ada96a85c635c4bb575eddb
kadirbelen <61799635+kadirbelen@users.noreply.github.com>
2022-07-18 16:11:52 +0300

    product searched by productTitle

--- routes/product/queryProductRouter.js
+++ routes/product/queryProductRouter.js
@@ -6,29 +6,38 @@ const router = express.Router();
 router.get("/getAll", async(req, res) => {
     const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
     const page = req.query.page ? parseInt(req.query.page) : 0;
+    const productName = req.query.productName ?
+        req.query.productName.toString() :
+        null;
 
     try {
-        var product = await Product.find()
-            .skip(page * pageSize)
-            .limit(pageSize);
-
+        var product;
+        if (productName === null) {
+            product = await Product.find()
+                .skip(page * pageSize)
+                .limit(pageSize);
+        } else {
+            var regex = new RegExp(productName, "i"); //case sensitive
+            product = await Product.find({
+                    productTitle: regex,
+                })
+                .skip(page * pageSize)
+                .limit(pageSize);
+        }
         res.json(product);
     } catch (error) {
-        res.json(err);
+        res.json(error);
     }
 });
 
-//endpoint productGetAll üzerinden yapılacak
-// router.get("/getByCategoryId/:id", (req, res) => {
-//     console.log(req.params.id);
-//     Product.find({ category: req.params.id })
-//         .then((product) => {
-//             console.log(product);
-//             res.json(product);
-//         })
-//         .catch((e) => {
-//             res.json(e);
-//         });
-// });
+router.get("/:id", async(req, res) => {
+    console.log(req.params.id);
+    try {
+        const product = await Product.findOne({ _id: req.params.id });
+        res.json(product);
+    } catch (error) {
+        res.json(error);
+    }
+});
 
 module.exports = router;
