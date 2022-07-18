29d180d1a1a7b0fdbaed944d077b1dfef5906dd2
kadirbelen <61799635+kadirbelen@users.noreply.github.com>
2022-07-18 16:43:26 +0300

    user role updated

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -2,33 +2,32 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 function verifyToken(req, res, next) {
-  const token = req.header("Authorization");
-  console.log(token);
-  //token var mı?
-  if (!token) {
-    res.status(401).send("Access denied. No token provided.");
-    return;
-  }
+    const token = req.header("Authorization");
+    console.log(token);
+    //token var mı?
+    if (!token) {
+        res.status(401).send("Access denied. No token provided.");
+        return;
+    }
 
-  jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
-    req.userId = decoded._id;
-    if (err) res.status(403).json("Token is not valid");
-    next();
-  });
+    jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+        req.userId = decoded._id;
+        if (err) res.status(403).json("Token is not valid");
+        next();
+    });
 }
 
 function verifyAndAuthorizationToken(req, res, next) {
-  verifyToken(req, res, () => {
-    User.findById(req.userId).then((user) => {
-      //admin mi?
-      if (user.role) {
-        next();
-      } else {
-        res.status(403).send("Invalid token or you have to be admin");
-        return;
-      }
+    verifyToken(req, res, () => {
+        User.findById(req.userId).then((user) => {
+            if (user.role === "admin") {
+                next();
+            } else {
+                res.status(403).send("Invalid token or you have to be admin");
+                return;
+            }
+        });
     });
-  });
 }
 
 module.exports = { verifyToken, verifyAndAuthorizationToken };
--- models/User.js
+++ models/User.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Product = require("./Product");
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, required: true, min: 3, max: 50 },
-  email: { type: String, require: true, min: 6, max: 255, unique: true },
-  password: { type: String, required: true, min: 6, max: 1024 },
-  role: { type: Boolean, default: false }, //true ise admin
-  date: { type: Date, default: Date.now },
+    userName: { type: String, required: true, min: 3, max: 50 },
+    email: { type: String, require: true, min: 6, max: 255, unique: true },
+    password: { type: String, required: true, min: 6, max: 1024 },
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+    date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("User", UserSchema);
